c4df5b6c7d213d4fd77afac698333d894f75f83f
nevosmic <nevosmic@post.bgu.ac.il>
2022-05-16 18:47:59 +0300

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
-
-function App() {
-  return <h1>Let's start!</h1>;
-}
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NewPlace from "./places/pages/NewPlace";
+import Users from "./user/pages/Users";
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact={true}>
+          <NewPlace />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;

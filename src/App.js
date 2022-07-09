141c377d55b0e855c252b78951bc9c74fa4237d2
nevosmic <nevosmic@post.bgu.ac.il>
2022-07-09 17:47:10 +0300

    Stay logged in after page refresh

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -14,19 +14,74 @@ import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import Authenticate from "./user/pages/Authenticate";
 import { AuthContext } from "./shared/components/context/auth-context";
 
+let logOutTimerId;
+
+//TODO: add a state isCheckingAuth=useState(true)-display a message: please wait.setIsCheckingAuth(false) inside useEffect
 const App = () => {
   const [token, setToken] = useState(false);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
   const [userId, setUserId] = useState(false);
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
+    //Set expiration date for the token
+    const myTokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60); //current date + 1h [(1000milisec*60=1min)*60=1h )]
+    console.log("myTokenExpirationDate: ", myTokenExpirationDate);
+    setTokenExpirationDate(myTokenExpirationDate);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiration: myTokenExpirationDate.toISOString(),
+      })
+    );
+    console.log(JSON.parse(localStorage.getItem("userData")));
   }, []);
 
   const logout = useCallback(() => {
+    // restart all states
     setToken(null);
+    setTokenExpirationDate(null);
     setUserId(null);
   }, []);
+
+  //token change if user login/logout
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      //user logged in
+
+      //calculate the remaining time - in Milisec
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logOutTimerId = setTimeout(logout, remainingTime);
+    } else {
+      //user logged out - manually or automatically
+      clearTimeout(logOutTimerId); //The clearTimeout() method requires the id returned by setTimeout() to know which setTimeout() method to cancel
+    }
+  }, [token, logout, tokenExpirationDate]);
+
+  /*CHECK USER ID FOR AUTO-LOGIN (stay logged in after page refresh)
+   runs only once, when the component mounts-renders for the first time*/
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      // we have a token and a VALID one so we can login
+      console.log("AUTO-LOGIN");
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
+    }
+  }, [login]);
 
   let routes;
   if (token) {
